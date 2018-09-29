3b9370582dc5fad6b0548b6499a20dd2068ebae8
dyzio18 <dyzio18@gmail.com>
2018-09-29 19:43:58 +0200

    Init

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package dyzio_theme
+ */
+
+get_header();
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
+
+			<section class="site__404 not-found">
+				<header class="page-header">
+					<h1>404</h1>
+				</header><!-- .page-header -->
+
+				<div class="page-content">
+	
+
+				</div><!-- .page-content -->
+			</section><!-- .error-404 -->
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
